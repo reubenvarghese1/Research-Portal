4caf668fbac867c256561cf98f292270985a4c65
Reuben Varghese <reubenvarghese1@gmail.com>
2018-01-24 19:20:38 +0530

    Update department.php

--- department.php
+++ department.php
@@ -5,7 +5,7 @@
 <title>Department | <?php echo $dep; ?></title>
 <link rel="shortcut icon" href="img/favicon.ico" type="image/png">
  <!--Import Google Icon Font-->
-      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
